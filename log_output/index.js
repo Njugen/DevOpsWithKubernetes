65122e920570bca2d4bffaffcb76d88ce602daba
thai <privat_thai_nguyen@hotmail.com>
2026-01-03 18:32:18 +0200

    Release 1.11

--- log_output/index.js
+++ log_output/index.js
@@ -3,36 +3,28 @@ const fs = require('fs');
 const path = '/usr/src/files';
 const fileName = 'log.txt';
 
-const port = Number(process.env.PORT) || 3000;
+const port = process.env.PORT || 3000;
 
-const generateText = () => {
+const getPingPongsText = () => {
+    let text = "Waiting, please reload in a moment...";
+    try {
+        text = fs.readFileSync(`${path}/${fileName}`, 'utf8');
+    } catch (err) {
+        console.error("Error reading file:", err);
+    }
+    return "Ping / Pongs: " + text;
+}
+
+const getStatusText = () => {
     const randomString = Math.random().toString(36).substring(2, 15);
     const timestamp = new Date().toISOString();
     return `${timestamp}: ${randomString}`;
 }
 
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end(getStatusText() + "\n" + getPingPongsText() + "\n");
+})
 
-if (port === 3000) {
-    const server = http.createServer((req, res) => {
-        let text;
-        try {
-            text = fs.readFileSync(`${path}/${fileName}`, 'utf8');
-        } catch (err) {
-            res.statusCode = 404;
-            res.setHeader("Content-Type", "text/plain");
-            res.end("Something went wrong. There might be an error in the file system.\n");
-            return;
-        }
-
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/plain");
-        res.end(text + "\n");
-    })
-
-    server.listen(port);
-} else if (port === 3001) {
-    setInterval(() => {
-        const text = generateText();
-        fs.writeFileSync(`${path}/${fileName}`, text);
-    }, 5000);
-}
+server.listen(port)
--- ping-pong/index.js
+++ ping-pong/index.js
@@ -1,19 +1,30 @@
 const http = require('http');
-
+const fs = require('fs');
+const path = '/usr/src/files';
+const fileName = 'log.txt';
 const port = process.env.PORT || 3000;
 
 let pongs = 0;
 
-const getPongs = () => {
-    const text = `Pong count: ${pongs}`;
-    pongs++;
-    return text;
+const getPingPongNumber = () => {
+    let number = 0;
+    try {
+        number = fs.readFileSync(`${path}/${fileName}`, 'utf8');
+    } catch (err) {
+        console.error("Error reading file:", err);
+    }
+    return parseInt(number);
 }
 
 const server = http.createServer((req, res) => {
+    if (pongs === 0) {
+        pongs = getPingPongNumber();
+    }
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
-    res.end(getPongs() + "\n");
+    res.end(`Pong count: ${pongs}` + "\n");
+    fs.writeFileSync(`${path}/${fileName}`, pongs.toString());
+    pongs++;
 })
 
 server.listen(port);
